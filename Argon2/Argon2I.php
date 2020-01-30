747a2cc6efcec547770bd7aabc4e9124bd6a008e
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-01-31 00:54:40 +0330

    Argon2I class created

---
+++ Argon2/Argon2I.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author  BlackPlatinum Developers
+ * @license MIT
+ * Date: 31/Jan/2020 00:53 AM
+ *
+ * Argon2I hasher class
+ **/
+
+namespace BlackPlatinum\Hashing\Argon2;
+
+
+class Argon2I
+{
+
+}
