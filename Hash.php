bcde8800ff5f974cd906900a82578d9a0e28256d
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-01-31 01:34:10 +0330

    Hash class created

---
+++ Hash.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author  BlackPlatinum Developers
+ * @license MIT
+ * Date: 31/Jan/2020 01:26 AM
+ *
+ * Main hasher class
+ **/
+
+namespace BlackPlatinum\Hashing;
+
+
+use BlackPlatinum\Hashing\BaseHasher\BaseHasher;
+
+
+class Hash
+{
+    //
+}
