bffe5158e5013c5097bddd4756f16caa569ca291
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-01-31 00:54:23 +0330

    Bcrypt class created

---
+++ Bcrypt/Bcrypt.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author  BlackPlatinum Developers
+ * @license MIT
+ * Date: 31/Jan/2020 00:52 AM
+ *
+ * Bcrypt hasher class
+ **/
+
+namespace BlackPlatinum\Hashing\Bcrypt;
+
+
+class Bcrypt
+{
+
+}
