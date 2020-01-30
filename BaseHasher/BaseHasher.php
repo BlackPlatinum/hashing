5dad30247a6c8430de91dd6346858c55770ca95c
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-01-31 00:18:20 +0330

    BaseHasher class created

---
+++ BaseHasher/BaseHasher.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author  BlackPlatinum Developers
+ * @license MIT
+ * Date: 31/Jan/2020 00:17 AM
+ *
+ * Base hasher class
+ **/
+
+namespace BlackPlatinum\Hashing\BaseHasher;
+
+
+class BaseHasher
+{
+    //
+}
