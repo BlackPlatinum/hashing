a1eb0a1b20d77ac4b91b0ae22865ce9073b2ef26
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-02-01 02:13:24 +0330

    HashException class created

---
+++ Core/Exceptions/HashException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author  BlackPlatinum Developers
+ * @license MIT
+ * Date: 1/Feb/2020 00:42 AM
+ *
+ * Main hasher class
+ **/
+
+namespace BlackPlatinum\Hashing\Core\Exceptions;
+
+use RuntimeException;
+
+class HashException extends RuntimeException
+{
+    //
+}
